726e7f5382dd8a6d515245f013d0e9d6e2539d97
SHUBHANK MAHESHWARI <maheshwarishubhank2002@gmail.com>
2025-12-29 15:21:35 +0530

    setting up project

---
+++ Backend/Server.js
@@ -0,0 +1,11 @@
+require('dotenv').config()
+const express=require('express')
+const app=express()
+
+app.use(express.json())
+
+
+const port=process.env.PORT
+app.listen(port,()=>{
+    console.log(`listening on port ${port}`)
+})
